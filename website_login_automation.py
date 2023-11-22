188d9b3918d828d5fe606948f95672dfe744e04e
fabe189 <fabe189@outlook.com>
2023-11-22 10:56:58 +0100

    next push

--- website_login_automation.py
+++ website_login_automation.py
@@ -1,6 +1,23 @@
+# import the Selenium library
+# Selenium has to be installed on the system  (CMD, "pip install Selenium")
+
 from selenium import webdriver
 import yaml
 
 conf = yaml.load(open('loginDetails.yml'))
-myFbEmail = conf['fb_user']['email']
-myFbPassword = conf['fb_user']['password']
+myEmail = conf['my_user']['email']
+myPassword = conf['my_user']['password']
+url = conf['my_user']['url']
+
+
+driver = webdriver.Chrome()
+
+def login(url,usernameId, username, passwordId, password, submit_buttonId):
+   driver.get(url)
+   driver.find_element_by_id(usernameId).send_keys(username)
+   driver.find_element_by_id(passwordId).send_keys(password)
+   driver.find_element_by_id(submit_buttonId).click()
+
+
+# calling the function "login" with the data
+login(url, "email", myEmail, "pass", myPassword, "loginbutton")
